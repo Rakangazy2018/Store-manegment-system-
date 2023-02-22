22dd8f3feda9c8ff24e98273d9a70679a2ea57dd
Rakangazy2018 <126001421+Rakangazy2018@users.noreply.github.com>
2023-02-22 14:40:15 +0300

    Add files via upload

---
+++ store/management/system/StoreManagementSystem.java
@@ -0,0 +1,16 @@
+
+package store.management.system;
+
+
+
+
+public class StoreManagementSystem {
+
+   
+    public static void main(String[] args) {
+     
+        
+        Helper.displayMenu();
+       
+    }
+}
